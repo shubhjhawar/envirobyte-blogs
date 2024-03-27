8eeda3113df8e3a661a3851b94063ebc62a1af76
Shubh <shubhjhawar78@gmail.com>
2024-03-26 20:20:11 -0400

    implemented the footer

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -49,7 +49,7 @@ export default function Header() {
           <nav className="hidden lg:flex lg:grow mt-2">
 
             {/* Desktop menu links */}
-            <ul className="flex grow justify-end items-center gap-6 lg:w-2/3">
+            <ul className="flex grow justify-end items-center gap-4 lg:w-2/3">
               <li className='flex flex-col items-center'>
                 <Link href="/" className="text-gray-600 hover:text-gray-900 px-3 py-2 flex flex-col items-center transition duration-150 ease-in-out">
                   {navbarIcons.home}
@@ -114,7 +114,7 @@ export default function Header() {
                 <UserLogin />
               </li>
               <li className="flex flex-col justify-center items-center">
-                <Link href="/signup" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
+                <Link href="/about/contact" className="btn-sm text-gray-200 bg-gray-900 hover:bg-gray-800 ml-3">
                   <span>Book a Demo</span>
                   <svg className="w-3 h-3 fill-current text-gray-400 shrink-0 ml-2 -mr-1" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.707 5.293L7 .586 5.586 2l3 3H0v2h8.586l-3 3L7 11.414l4.707-4.707a1 1 0 000-1.414z" fillRule="nonzero" />
